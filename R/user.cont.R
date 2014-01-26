f464f96b74eea6db07273beb6d456402da310246
Maxime Hervü¾¼ <mx.herve@gmail.com>
2014-01-26 10:22:35 +0000

    version 0.9-31

--- R/user.cont.R
+++ R/user.cont.R
@@ -1,6 +1,6 @@
 user.cont <- function(cont) {
   user.cont.lsmc <- function(levs,...) {
-    M <- as.data.frame(cont)
+    M <- as.data.frame(t(cont))
     for (i in 1:ncol(M)) {
 	colnames(M)[i] <- paste(paste(rownames(M)[M[,i]>0],collapse="-"),
 	  paste(rownames(M)[M[,i]<0],collapse="-"),sep=" vs ")
